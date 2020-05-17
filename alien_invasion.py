07d8471506d3e3bd15f00f2b5d65d71c60bcc6e2
merlin2181 <merlin2181@gmail.com>
2020-05-16 21:23:03 -0700

    Alien Invasion main file
    Added a scoreboard to the top of the screen that includes player score,
    high score, level and ships remaining.

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from time import sleep
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -18,13 +19,16 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-            # (self.settings.screen_width, self.settings.screen_height)) - use to start game in a window. Dimensions come from Settings module.
+        self.screen = pygame.display.set_mode ((self.settings.screen_width, self.settings.screen_height))
+            # pygame.FULLSCREEN) - use
+            #  to start game in a window. Dimensions come from Settings module.
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
-        # Create an instance to store game statistics.
+        # Create an instance to store game statistics,
+        #   and create a scoreboard.
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
         # Links to Ship class in the ship module
         self.ship = Ship(self)
         # Links to Bullet class in the bullet module
@@ -65,9 +69,15 @@ class AlienInvasion:
         """Start a new game when the Player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
+
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Hide the mouse cursor.
             pygame.mouse.set_visible(False)
@@ -122,11 +132,22 @@ class AlienInvasion:
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
+        
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create a new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
+
+            # Increase Level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
@@ -155,8 +176,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships left.
+            # Decrement ships left and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -197,7 +219,7 @@ class AlienInvasion:
         alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
-        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        alien.rect.y = alien_height + 2 * alien_height * row_number
         self.aliens.add(alien)
     
     def _check_fleet_edges(self):
@@ -225,6 +247,9 @@ class AlienInvasion:
         
         # Draw alien(s) on the screen.
         self.aliens.draw(self.screen)
+
+        # Draw the score information.
+        self.sb.show_score()
         
         # Draw the play button if the game is inactive.
         if not self.stats.game_active:
